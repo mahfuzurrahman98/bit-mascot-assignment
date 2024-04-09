436446dc41722500f7b82d6d37f86837283c6a20
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-10 03:46:19 +0600

    set status default value to 1, and role default value to 2

---
+++ database/migrations/2024_04_09_190639_set_status_and_role_default_value_in_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::table('users', function (Blueprint $table) {
+            // set status and role's default value
+            $table->tinyInteger('status')->default(1)->change();
+            $table->tinyInteger('role')->default(2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
