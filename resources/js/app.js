d98552b6f863f3db0821057c4fbda0d951a53def
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-11 02:06:46 +0600

    disable submit button, when any form is submitted

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,11 @@
 import "bootstrap";
 import $ from "jquery";
 window.$ = window.jQuery = $;
+
+// when any form is submitted, disable the submit button and change the text to "Processing...
+$(document).on("submit", "form", function () {
+    $(this)
+        .find("button[type=submit]")
+        .attr("disabled", true)
+        .text("Processing...");
+});
